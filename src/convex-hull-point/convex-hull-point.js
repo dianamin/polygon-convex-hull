edfb19f06f08c63b7fc820a7ac6a1ee19f844fd0
Diana Ghinea <97.diana@gmail.com>
2016-12-15 20:11:10 +0200

    added points

--- src/convex-hull-point/convex-hull-point.js
+++ src/convex-hull-point/convex-hull-point.js
@@ -2,10 +2,41 @@ Polymer({
   is: 'convex-hull-point',
 
   properties: {
-    prop1: {
+    circle: {
+      type: Object,
+    },
+
+    index: {
+      type: Number,
+    },
+
+    cx: {
+      type: Number,
+    },
+
+    cy: {
+      type: Number,
+    },
+
+    _namespace: {
       type: String,
-      value: 'convex-hull-point',
+      value: 'http://www.w3.org/2000/svg',
     },
   },
+
+  build: function(svg, cx, cy, index) {
+    this.index = index;
+    this.cx = cx;
+    this.cy = cy;
+    this.circle = document.createElementNS(this._namespace, 'circle');
+    d3.select(this.circle).attr('cx', cx);
+    d3.select(this.circle).attr('cy', cy);
+    d3.select(this.circle).attr('r', 0);
+    this.circle.classList.add('convex-hull-point');
+    this.circle.classList.add('circle');
+    svg.append(this.circle);
+    console.log(d3.select(this.circle).attr('r'));
+    d3.select(this.circle).transition().duration(300).attr("r", 15);
+  }
 
 });
--- src/convex-hull-space/convex-hull-space.js
+++ src/convex-hull-space/convex-hull-space.js
@@ -2,10 +2,31 @@ Polymer({
   is: 'convex-hull-space',
 
   properties: {
-    prop1: {
-      type: String,
-      value: 'convex-hull-space',
+    simulation: {
+    	type: Object,
     },
+
+    points: {
+    	type: Array,
+    },
+  },
+
+  create: function() {
+    this.set('points', []);
+  },
+
+  ready: function() {
+  	this.simulation = d3.forceSimulation();
+  	this.$.svg.addEventListener('dblclick', (event) => {
+  		this._addPoint(event.clientX, event.clientY);
+  	});
+  },
+
+  _addPoint: function(x, y) {
+  	var newPoint = document.createElement('convex-hull-point');
+    this.append(newPoint);;
+  	newPoint.build(this.$.svg, x, y, 0);
+    //this.push('points', newPoint);
   },
 
 });
